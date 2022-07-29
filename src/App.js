8e96a2b4502bb8eb658acb9f377cb5ba9d728973
tjdcjf0222 <tjd000222@naver.com>
2022-07-29 20:24:24 +0900

    first-commit (BulletinBoard-home)
    Co-authored-by: James Jung <therealjamesjung@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
-function App() {
-  return (
+const App = () => {
+    const [boardContent, setBoardContent] = useState({
+        title : '',
+        content: ''
+    })
+
+    const [viewContent, setViewContent] = useState([]);
+
+    const handleTitle = (event) => {
+      const data = event.target.value;
+      setBoardContent({
+        ...boardContent,
+        title: data
+      })
+      console.log(boardContent);
+    }
+
+    const handleContent = (event) => {
+      const data = event.target.value;
+      setBoardContent({
+        ...boardContent,
+        content: data
+      })
+      console.log(boardContent);
+    }
+    
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Free Board</h1>
+      <div >
+        {viewContent.map(element => 
+            <div className='board-container'>
+                <h2>{element.title}</h2>
+                <div>
+                    {element.content}
+                </div>
+            </div>
+        )}
+      </div>
+      <div className='form-wrapper'>
+        <input input className='title-input'
+        type='text'
+        placeholder='제목'
+        onChange={handleTitle}
+        name='title' />
+        
+        <textarea className="text-area" placeholder='내용' onChange={handleContent}></textarea>
+      </div>
+      <button className="submit-button"
+      onClick={() => {
+        setViewContent(viewContent.concat({...boardContent}));
+      }}>입력</button>
     </div>
   );
 }
