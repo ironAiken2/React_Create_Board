6215a90c3c6cfa83c796b8033cc4d5797f7fc8f2
tjdcjf0222 <tjd000222@naver.com>
2022-07-31 20:51:44 +0900

    [Add] local storage에 회원정보 저장 및 로그인시 회원정보 조회 기능 구현

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,5 @@
 import "./Login.css";
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
@@ -7,29 +7,28 @@ const Login = () => {
   const navigate = useNavigate();
 
   // input data 의 변화가 있을 때마다 value 값을 변경해서 useState 해준다
-  const state = {
-    id: "",
-    pw: "",
-  };
-
-  const handleInputId = (e) => {
-    this.useState({
-      state,
-      id: e.target.value,
-    });
-  };
-
-  const handleInputpw = (e) => {
-    this.useState({
-      state,
-      pw: e.target.value,
-    });
-  };
+  const [id, InputId] = React.useState();
+  const onChangeId = (event) => {
+    console.log(event.target.value);
+    InputId(event.target.value);
+  }
+  
+  const [pw, InputPw] = React.useState();
+  const onChangePw = (event) => {
+    console.log(event.target.value);
+    InputPw(event.target.value);
+  }
 
   // login 버튼 클릭 이벤트
   const onClickLogin = () => {
     console.log("click login");
-    navigate("/Main");
+
+    if(localStorage.getItem(id) && localStorage.getItem(id) === pw){
+      alert("로그인 성공!");
+      navigate("/Main");
+    } else {
+      alert("로그인 실패! 아이디 또는 비밀번호가 일치하지 않습니다.");
+    }
   };
 
   // 페이지 렌더링 후 가장 처음 호출되는 함수
@@ -40,28 +39,28 @@ const Login = () => {
       <div>
         <label htmlFor="input_id"> ID : </label>
         <input
-          input
+          value={id}
           type="text"
           name="input_id"
           placeholder=""
-          value={useState.id}
-          onChange={handleInputId}
+          onChange={onChangeId}
         />
       </div>
       <div>
         <label htmlFor="input_pw">PW : </label>
         <input
+          value={pw}
           type="password"
           name="input_pw"
           placeholder=""
-          value={useState.pw}
-          onChange={handleInputpw}
+          onChange={onChangePw}
         />
       </div>
       <div>
         <button onClick={onClickLogin}>Login</button>
       </div>
       <div className="sign-up">
+        회원이 아니시라면? 　　
         <Link to="/SignUp">회원가입</Link>
       </div>
     </div>
--- src/SignUp.js
+++ src/SignUp.js
@@ -5,26 +5,72 @@ import { useNavigate } from "react-router-dom";
 const SignUp = () => {
   const navigate = useNavigate();
 
+  const [id, setId] = React.useState();
+  const onChangeId = (event) => {
+    console.log(event.target.value);
+    setId(event.target.value);
+  }
+  
+  const [pw, setPw] = React.useState();
+  const onChangePw = (event) => {
+    console.log(event.target.value);
+    setPw(event.target.value);
+  }
+
+  const [name, setName] = React.useState();
+  const onChangeName = (event) => {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+
   const onClickSignIn = () => {
     console.log("click login");
-    navigate("/");
+
+    if(localStorage.getItem(id)){
+      alert("중복된 아이디입니다!.");
+    } else {
+      localStorage.setItem(name, id);
+      localStorage.setItem(id , pw);
+
+      alert("회원가입 성공!");
+      navigate("/");
+    }
   };
 
+  
   return (
     <div className="SingUp-container">
       <h1>BulletinBoard</h1>
       <h2>Sign-Up</h2>
       <div>
         <label htmlFor="input_id"> ID : </label>
-        <input input type="text" name="input_id" placeholder="" />
+        <input
+         value = {id} 
+         input type="text" 
+         name="input_id"
+         placeholder="" 
+         onChange={onChangeId}
+         />
       </div>
       <div>
         <label htmlFor="input_pw">PW : </label>
-        <input type="password" name="input_pw" placeholder="" />
+        <input 
+        value = {pw}
+        type="password" 
+        name="input_pw" 
+        placeholder="" 
+        onChange={onChangePw}
+        />
       </div>
       <div>
-        <label htmlFor="input_pw">Name : </label>
-        <input type="password" name="input_pw" placeholder="" />
+        <label htmlFor="input_Name">Name : </label>
+        <input 
+        value = {name}
+        type="text" 
+        name="input_Name" 
+        placeholder="" 
+        onChange={onChangeName}
+        />
       </div>
       <div>
         <button onClick={onClickSignIn}>Sign In</button>
